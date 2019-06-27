7aa1a190e0a3f38575c68cfc675d1484babcf4b4
github username <github email address>
2019-06-27 07:44:51 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
+  BRANDS = []
+
+  def initialize(brand) # instead of having a separate setter method....since the instance is already going to be initialized with the brand and is going to be set to @brand...we can just do it on instantiation. 
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
